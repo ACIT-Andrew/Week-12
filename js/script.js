bf0e3549749c1c1017cbb7e881596d7cd0c1d350
ACIT-Andrew <115048179+ACIT-Andrew@users.noreply.github.com>
2022-11-22 14:56:31 -0800

    Button addEventListener

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
+const print = console.log;
+
+const b1 = document.querySelector('.b1');
+print(b1);
+
+function linkme(){
+    alert("helooo");
+    // window.open("https://www.youtube.com");
+    // b1.removeEventListener("click", linkme);
+}
+
+b1.addEventListener('click', linkme), {once:true};
 
